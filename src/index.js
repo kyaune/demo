ad5a219458e156efd0cb3238791c61bcfb70a0c4
kyaune <kyaune@gmail.com>
2019-04-15 08:07:51 +0300

    2nd component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './Header';
+import Description from './Description';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Header />, <Description />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
